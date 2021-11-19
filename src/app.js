d62344c4dfaaa813cb7426649b81ead2d42910e6
Anil Wijesinghe <anilanjuna@gmail.com>
2021-11-19 08:20:00 +0000

    unit test for passing with coffee name

--- src/app.js
+++ src/app.js
@@ -3,5 +3,9 @@ const app = express();
 
 app.get('/', (req, res) => res.send('Welcome to the Drinks API!'));
 app.get('/coffeeLover', (req, res) => res.send('I like coffee!'));
+app.get('/coffee', (req, res) => res.json({
+    drinkType: 'Coffee',
+    name: 'Latte',
+}));
 
 module.exports = app;
