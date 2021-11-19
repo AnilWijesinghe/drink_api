cee1148921c1a9d79cf00af48fda001015dd40f4
Anil Wijesinghe <anilanjuna@gmail.com>
2021-11-19 08:51:08 +0000

    unit test for passing parameter coffee Cappuccino

--- src/controllers/coffee_request.test.js
+++ src/controllers/coffee_request.test.js
@@ -4,6 +4,17 @@ describe('Test coffee API endpoint request', () => {
     test('GET /coffee_request should return correct message', async () => {
         const res = await request(app).get('/coffee_request');
         expect(res.statusCode).toEqual(200);
-        expect(res.text).toEqual('I like coffee, Macchiato!');
+        expect(res.text).toEqual('I like coffee, Cappuccino!');
     });
+    test('GET /coffee with different param should return correct object',
+        async () => {
+            const res = await request(app)
+                .get('/coffee')
+                .query({ coffeeName: 'Cappuccino' });
+            expect(res.statusCode).toEqual(200);
+            expect(res.body).toEqual({
+                drinkType: 'Coffee',
+                name: 'Cappuccino',
+            });
+        });
 });
