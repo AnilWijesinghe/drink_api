789c2756a7045acce06692747f5c462e3f4acbbb
Anil Wijesinghe <anilanjuna@gmail.com>
2021-11-19 08:31:49 +0000

    unit test with route coffee request

--- src/app.js
+++ src/app.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const app = express();
+const routes = require('./routes/routes');
+app.use(express.json());
 
 app.get('/', (req, res) => res.send('Welcome to the Drinks API!'));
-app.get('/coffeeLover', (req, res) => res.send('I like coffee!'));
-app.get('/coffee', (req, res) => res.json({
-    drinkType: 'Coffee',
-    name: 'Latte',
-}));
-
+app.get('/coffeelover', (req, res) => res.send('I like coffee!'));
+app.use('/', routes);
 module.exports = app;
